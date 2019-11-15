82b340cf94b6f33dce9e04fc6a227ee038da0740
Fabian Brand HSLU <fabian.brand@stud.hslu.ch>
2019-11-15 10:30:27 +0100

    add oracle application

--- OracleApplication/src/main/java/ch/hslu/wipro/prototyp/oracle/OracleApplication.java
+++ OracleApplication/src/main/java/ch/hslu/wipro/prototyp/oracle/OracleApplication.java
@@ -1,4 +1,36 @@
 package ch.hslu.wipro.prototyp.oracle;
 
-public class OracleApplication {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class OracleApplication {
+    private static final Logger LOGGER = LogManager.getLogger(OracleApplication.class);
+
+    /**
+     * Privater Konstruktor.
+     */
+    private OracleApplication() {
+    }
+
+    /**
+     * Main-Methode.
+     * @param args Startargumente.
+     */
+    public static void main(final String[] args) throws InterruptedException {
+        LOGGER.info("Initiate new qubic");
+
+        new Thread(new Runnable() {
+            public void run() {
+                new OracleInstance("Fabian");
+            }
+        }).start();
+
+        while (true) {
+            try {
+                Thread.sleep(60000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
